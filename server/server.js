9bdea0352729d102291a9937af61bdba706c4083
ruben <krasnoschek003@gmail.com>
2023-02-15 22:42:19 +1000

    create simple server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+/**
+ * map users. Contains elements of the form:
+ *  {userName : "", password : ""}
+ */
+const users = {};
+
+/**
+ * map of rooms. Contains elements of the form:
+ *  { roomName : "", description : "", maxPeople : 99,
+ *      private : true|false, creator : "",
+ *      users : [
+ *          <username> : {userName : ""}, ...
+ *      ]
+ *  }
+ */
+const rooms = {};
+
+//create http-server, wrapped in socket.io
+const io = require('socket.io')(require('http')
+    .createServer(
+        function(){}
+    ).listen(80)
+);
